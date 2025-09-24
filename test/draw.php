9aa1c17e6624880d210dd3711b71858fe85b1d8a
kllxs <1164601554@qq.com>
2025-09-24 09:01:55 +0800

    Update draw.php

--- test/draw.php
+++ test/draw.php
@@ -25,30 +25,55 @@ Window::onClosing($window, function ($window) {
     return 1;
 });
 
+// 方块位置
+$drawX = 50;
+$drawY = 50;
 
+$start_drop = false; // 可拖到状态
 
 // 创建绘画处理程序
 $areaHandler = Area::handler(
-    function ($handler, $area, $params) { // 绘画处理程序
+    function ($handler, $area, $params) use (&$drawX, &$drawY) { // 绘画处理程序
         // 创建红色笔刷
         $redBrush = Draw::createBrush(DrawBrushType::Solid, 1.0, 0.0, 0.0, 1.0);
         // 创建方块路径
         $drawPath = Draw::createPath(DrawFillMode::Winding);
         // 绘制一个红色的方块
-        Draw::pathAddRectangle($drawPath, 50, 50, 100, 100);
+        Draw::pathAddRectangle($drawPath, $drawX, $drawY, 100, 100);
         // 结束路径定义
         Draw::pathEnd($drawPath);
         // 填充方块
         Draw::fill($params, $drawPath, $redBrush);
     },
     function ($handler, $area, $keyEvent) { // 按键事件
-        var_dump($area, $keyEvent);
+        /* var_dump($area, $keyEvent);
         echo "按键事件";
+        return 1; */
     },
-    function ($handler, $area, $mouseEvent) { // 鼠标事件
-        var_dump($area, $mouseEvent);
-        echo "鼠标事件";
-    },
+    function ($handler, $area, $mouseEvent) use (&$drawX, &$drawY, &$start_drop) { // 鼠标事件
+        var_dump($mouseEvent);
+        if ($mouseEvent->Down == 1) { // 鼠标按下
+            $start_drop = true; // 可拖到
+        }
+        if (
+            $start_drop &&
+            $mouseEvent->Held1To64 == 1 &&
+            $mouseEvent->X <= $drawX + 100 &&
+            $mouseEvent->Y <= $drawY + 100
+        ) { // 判断是否在方块内，是否可拖到状态，是否长按状态
+            $drawX = $mouseEvent->X - 50;
+            $drawY = $mouseEvent->Y - 50;
+            // 队列重绘
+            Area::queueRedraw($area);
+        }
+        if ( // 鼠标松开,长按松开
+            $mouseEvent->Held1To64 == 0 &&
+            $mouseEvent->Down == 0
+        ) {
+            $start_drop = false; // 取消可拖到状态
+        }
+        // echo "鼠标事件";
+    }
 );
 
 // 创建绘画区域
